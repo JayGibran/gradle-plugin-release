3a73460d155708ec5a5afb70329baa9add9d446c
jaygibran <jay.gibran@sonalake.com>
2022-12-02 11:31:00 +0000

    first commit

---
+++ src/main/java/sonalake/poc/gradle_plugin/GradlePluginReleaseApplication.java
@@ -0,0 +1,13 @@
+package sonalake.poc.gradle_plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePluginReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlePluginReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sonalake/poc/gradle_plugin/GradlePluginReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package sonalake.poc.gradle_plugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlePluginReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
